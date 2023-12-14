61492fb53674cec6b58366e55682c56b6af968f9
kota kesavarao <125714877+satyaan9951@users.noreply.github.com>
2023-12-14 22:21:40 +0530

    Update app.js5

--- app.js
+++ app.js
@@ -36,22 +36,22 @@ const authenticateToken = (request, response, next) => {
   const authHeader = request.headers['authorization']
   if (authHeader !== undefined) {
     jwtToken = authHeader.split(' ')[1]
-    if (jwtToken === undefined) {
-      response.status(401)
-      response.send('Invalid JWT Token')
-    } else {
-      jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
-        if (error) {
-          response.status(401)
-          response.send('Invalid JWT Token')
-        } else {
-          request.payload = payload
-          request.tweetId = tweetId
-          request.tweet = tweet
-          next()
-        }
-      })
-    }
+  }
+  if (jwtToken === undefined) {
+    response.status(401)
+    response.send('Invalid JWT Token')
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        response.status(401)
+        response.send('Invalid JWT Token')
+      } else {
+        request.payload = payload
+        request.tweetId = tweetId
+        request.tweet = tweet
+        next()
+      }
+    })
   }
 }
 //API1
